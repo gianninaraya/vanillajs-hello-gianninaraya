b6a569faee53ea228edb51f6f9303792a07d4d24
Giannina Araya Aguilera <giannina.arayaa@gmail.com>
2026-01-05 21:14:12 -0300

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
+  
+  base: '/vanillajs-hello-gianninaraya/',
+  
   build: {
     // Specify the output directory for the build. This path is relative to your project root.
     outDir: '../dist',
